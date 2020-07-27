56b7d80a732454a87368bf53122cc03472b3cc10
Kleber Pereira <klebinho.pk@hotmail.com>
2020-07-27 14:14:36 -0300

    Add files via upload

---
+++ precache-manifest.501bb42e1064afbdaa24bc3cd4ad6201.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8776cb74ce9d27775867360b40242345",
+    "url": "./index.html"
+  },
+  {
+    "revision": "30e17888b066a19f2502",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "08aa58d2f821ba5292bf",
+    "url": "./static/css/main.8ccf7f84.chunk.css"
+  },
+  {
+    "revision": "30e17888b066a19f2502",
+    "url": "./static/js/2.e690d01f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e690d01f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08aa58d2f821ba5292bf",
+    "url": "./static/js/main.525f0c6b.chunk.js"
+  },
+  {
+    "revision": "5e8b18f29a5773c08a48",
+    "url": "./static/js/runtime-main.e45d693e.js"
+  }
+]);
